60fd359c0c8b99ce2c6c5d0b797e459459d55e24
Kevin  Watke <kevinwatke@Kevins-MacBook-Pro.local>
2021-05-13 13:20:07 -0500

    remove live preview when a user types into input form for a new todo

--- public/app.js
+++ public/app.js
@@ -1,10 +1,6 @@
 const inputForm = document.querySelector('#todo-input');
 const preview = document.querySelector('#preview');
 
-// Live preview. Copy the value of the input form into element for user to see as they type.
-inputForm.addEventListener('input', (e) => {
-  preview.innerHTML = `<span>${inputForm.value}</span>`
-});
 
 
 
